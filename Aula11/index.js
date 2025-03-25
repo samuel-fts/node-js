50ba89d442f922ed6ec518a992f00d14dd7243b5
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-03-25 10:40:56 -0300

    novos comentarios

--- Aula11/index.js
+++ Aula11/index.js
@@ -1,10 +1,10 @@
 const http=require('http');
-const eventos= require('events');
-const EventoEmissor = new eventos.EventEmitter();
+const eventos= require('events')//fez uma requisicao do objeto global events
+const EventoEmissor = new eventos.EventEmitter();//instanciou um novo objeto pegando o metodo eventEmitter
 
-const final = ()=>{console.log('fim do processo')};
+const final = ()=>{console.log('fim do processo')};//criou uma funcao que sera usada depois
 
-EventoEmissor.on('msg',()=>{console.log('curso de node')});
+EventoEmissor.on('msg',()=>{console.log('curso de node')});//.on atribui uma chave para ser chamada depois e executar oque espera do segundo parametro
 EventoEmissor.on('fim',final);
 
 
@@ -12,7 +12,7 @@ const porta=process.env.PORT || 3000;
 const retorno=()=>{console.log('servidor rodando')};
 
 const servidor=http.createServer((req,res)=>{
-    EventoEmissor.emit('msg');
+    EventoEmissor.emit('msg');//.emit, chama a emissao de evento criada anteriormente, usando a chave atribuida em seu parametro
     res.writeHead(200,{'Content-type':'text/plain'});
     res.write('CFBcurso');
     EventoEmissor.emit('fim');
